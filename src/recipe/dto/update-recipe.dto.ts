6d33b3c8c189ac8acea9ff25c3043d99ee53661c
Vasgan <gean.vasconez@gmail.com>
2024-06-05 23:30:36 -0500

    modify recipe dtos

--- src/recipe/dto/update-recipe.dto.ts
+++ src/recipe/dto/update-recipe.dto.ts
@@ -1,6 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateRecipeDto } from './create-recipe.dto';
 
-export class UpdateRecipeDto extends PartialType(CreateRecipeDto) {
-    id: number;
-}
+export class UpdateRecipeDto extends PartialType(CreateRecipeDto) {}
