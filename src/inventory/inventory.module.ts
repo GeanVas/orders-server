36d78e870ee659efd352f0702d129777a19e8e3d
GeanVas <gean.vasconez@gmail.com>
2024-06-24 20:07:21 -0500

    import database module

--- src/inventory/inventory.module.ts
+++ src/inventory/inventory.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { InventoryService } from './inventory.service';
 import { InventoryController } from './inventory.controller';
 import { inventoryProviders } from './inventory.providers';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+    imports: [DatabaseModule],
     controllers: [InventoryController],
     providers: [InventoryService, ...inventoryProviders],
 })
